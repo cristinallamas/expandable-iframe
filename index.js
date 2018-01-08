475269f469b6d11069d8ea2bcc62cf2d445126d4
Cristina Llamas <cristina_roman@dennis.co.uk>
2018-01-08 16:49:32 +0000

    collapse function now exists

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
-module.exports = function iframeExpand(transition,distance) {
-  // Select id, iframe and containerNode.
-  var id = window.frameElement.id;
-  var iframe = window.parent.document.getElementById(id);
-  var containerNode = (iframe === null) ? null : iframe.parentNode;
+'use strict';
+// Grab id from iframe.
+const id = window.frameElement.id;
+// Greab the iframe.
+const iframe = window.parent.document.getElementById(id);
+// Store the value of the initial height of the iframe.
+const initialIframeHeight = iframe.style.height;
 
-  if (containerNode === null) return null;
-  containerNode.style.height = distance;
-  iframe.style.height = distance;
-  iframe.style.transition = transition;
-  containerNode.style.transition = transition;
+var Expander = {
+  // Function that expands iframe up to the height defined in distance.
+  expand: function(transition,distance) {
+    var containerNode = (iframe === null) ? null : iframe.parentNode;
+    if (containerNode === null) return null;
+    containerNode.style.height = distance;
+    iframe.style.height = distance;
+    iframe.style.transition = transition;
+    containerNode.style.transition = transition;
+  },
+
+  // Function that collapses the iframe to its initial state.
+  collapse: function(transition){
+    var containerNode = (iframe === null) ? null : iframe.parentNode;
+    if (containerNode === null) return null;
+    containerNode.style.height = initialIframeHeight;
+    iframe.style.height = initialIframeHeight;
+    iframe.style.transition = transition;
+    containerNode.style.transition = transition;
+  }
+
 };
+
+module.exports = Expander;
